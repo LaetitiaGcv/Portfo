922ecd767bb95661c0c9dbc8af59b5502b4a2ed9
Laetitia Gcv <laetitia.gcv@gmail.com>
2020-07-22 21:53:14 +0200

    add authentication and admin side

---
+++ src/Form/ExperiencesType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Experiences;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ExperiencesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('school')
+            ->add('degree')
+            ->add('start_date')
+            ->add('end_start')
+            ->add('logo')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Experiences::class,
+        ]);
+    }
+}
