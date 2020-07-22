bc44f62649139631c0da1b627a5af8b727184a85
Laetitia Gcv <laetitia.gcv@gmail.com>
2020-07-22 22:41:05 +0200

    add login on admin side

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,6 +4,8 @@
 namespace App\Controller;
 
 
+use App\Repository\UserRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,6 +18,26 @@ class AdminController extends AbstractController
      */
     public function index():Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+
+        $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'User tried to access a page without having ROLE_ADMIN');
+
         return $this->render('admin/index.html.twig');
     }
+
+    /**
+     * @Route("/user", name="user")
+     * @param UserRepository $user
+     * @return Response
+     */
+    public function userList(UserRepository $user)
+    {
+        return $this->render('admin/user.html.twig', [
+            'user' => $user->findAll(),
+        ]);
+    }
+
+
+
 }
